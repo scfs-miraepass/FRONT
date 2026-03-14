8d7d0933a7c9f586fb9387a14108c2846d80e7d7
rldn12134 <rldn12134@rldn.xyz>
2026-03-14 21:14:58 +0900

    feat: 로그인 검증 미들웨어 로직 재구성

--- app/middleware/auth.global.ts
+++ app/middleware/auth.global.ts
@@ -2,15 +2,27 @@ import { getCurrentUserAuthGet } from "@/sdk"
 
 export default defineNuxtRouteMiddleware(async (to, from) => {
     if (import.meta.client) {
+        const session = useSession()
+
+        // 이미 세션 정보가 있다면 API 호출 없이 통과
+        if (session.value) {
+            if (to.path.startsWith("/login")) {
+                return navigateTo("/")
+            }
+            return
+        }
+
         const req = await getCurrentUserAuthGet()
+
         if (to.path.startsWith("/login")) {
             if (!req.error) {
+                session.value = req.data.data
                 return navigateTo("/")
             }
         }
         if (req.error) {
             return navigateTo("/login", { replace: true })
         }
-        useSession().value = req.data.data
+        session.value = req.data.data
     }
 })
