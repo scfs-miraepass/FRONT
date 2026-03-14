894e2c9a0431f9988f5b25574ef8a2a04a49f412
rldn12134 <rldn12134@rldn.xyz>
2026-03-14 13:57:07 +0900

    chore: motion-v/nuxt module install

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -19,7 +19,7 @@ export default defineNuxtConfig({
         },
     },
 
-    modules: ['@nuxt/fonts', '@nuxtjs/device'],
+    modules: ['@nuxt/fonts', '@nuxtjs/device', 'motion-v/nuxt'],
 
     fonts: {
         families: [
