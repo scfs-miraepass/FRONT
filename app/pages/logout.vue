bfd996f50585c5fb9d8f93a7259e316ec752eeb9
rldn12134 <rldn12134@rldn.xyz>
2026-03-14 21:28:24 +0900

    feat: 로그아웃 기능 추가

---
+++ app/pages/logout.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { logoutAuthLogoutPost } from "@/sdk";
+
+const session = useSession()
+
+onMounted(async () => {
+    const req = await logoutAuthLogoutPost()
+    if (!req.error) {
+        session.value = undefined
+        await navigateTo("/login", { replace: true })
+    }
+})
+</script>
+
+<template>
+    <div class="w-screen h-screen flex items-center justify-center text-p1">
+        <USkeleton class="bg-transparent flex flex-col items-center">
+            <LogoSymbol class="mb-2" />
+            로그아웃 중이에요.. 잠시만 기다려주세요.
+        </USkeleton>
+    </div>
+</template>
