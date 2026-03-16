0005e371c5676a9953a8787005aeb9210c48e052
rldn12134 <dyddns101213@naver.com>
2026-03-16 05:53:20 +0000

    chore: @vite-pwa/nuxt module install

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -19,7 +19,7 @@ export default defineNuxtConfig({
         },
     },
 
-    modules: ['@nuxt/fonts', '@nuxtjs/device', 'motion-v/nuxt', '@nuxt/ui'],
+    modules: ['@nuxt/fonts', '@nuxtjs/device', 'motion-v/nuxt', '@nuxt/ui', '@vite-pwa/nuxt'],
 
     fonts: {
         families: [
