34cd83e682f4c80bc194064ad8026d4ddbd9a03b
rldn12134 <rldn12134@rldn.xyz>
2026-03-19 00:33:04 +0900

    init: 교사 메인페이지 생성

--- app/components/home/teacher/index.vue
+++ app/components/home/teacher/index.vue
@@ -1,22 +1,19 @@
 <script setup lang="ts">
-import Point from "./point.vue";
-import History from "./history/index.vue";
+
 
 const session = useSession()
 </script>
 
 <template>
-    <div class="w-screen h-screen p-5 flex flex-col" v-if="session?.type == 'student'">
+    <div class="w-screen h-screen p-5 flex flex-col" v-if="session?.type == 'teacher'">
         <div class="w-full flex items-center justify-between opacity-50 -mt-2">
-            <p class="text-ui-p1 font-medium text-black/50">{{ session.id }} {{ session.name }}</p>
+            <p class="text-ui-p1 font-medium text-black/50">{{ session.name }} 선생님</p>
             <NuxtLink to="/setting">
                 <UButton color="neutral" variant="ghost" icon="i-ph-gear-fill" class="p-2 rounded-2xl hover:bg-accented active:bg-accented focus-visible:bg-accented" size="xl" />
             </NuxtLink>
         </div>
         <div class="pb-2 mt-2.5 flex-1 flex flex-col">
-            <Point />
-            <hr class="my-3 border-default" />
-            <History />
+            ...
         </div>
     </div>
 </template>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import Student from "@/components/home/student/index.vue";
+import Teacher from "@/components/home/teacher/index.vue";
 </script>
 
 <template>
     <Student />
+    <Teacher />
 </template>
 
 <style scoped>
