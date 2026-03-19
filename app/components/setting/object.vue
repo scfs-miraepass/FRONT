ea56f152aec66e297cb01e87aa2594c642066cec
기우12134 <rldn12134@rldn.xyz>
2026-03-19 15:20:05 +0000

    Feat/setting

---
+++ app/components/setting/object.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+defineProps<{
+    icon?: string
+    icon_back?: string
+    label: string
+    value?: string
+}>()
+</script>
+
+<template>
+    <button class="w-full rounded-xl flex items-center justify-between gap-2 px-5.5 py-3 transition-all active:scale-95 active:opacity-50 active:bg-accented">
+        <div class="flex items-center text-p1">
+            <UIcon :name="icon" v-if="icon" class="mr-2" />
+            {{ label }}
+        </div>
+        <div class="flex items-center light:text-black/50 dark:text-white/50 text-ui-p2">
+            {{ value }}
+            <UIcon :name="icon_back" v-if="icon_back" class="ml-1" />
+        </div>
+    </button>
+</template>
---
+++ app/components/setting/section.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+defineProps<{
+    name?: string
+}>()
+</script>
+
+<template>
+    <div class="light:bg-default dark:bg-muted rounded-2xl px-0.5 py-2">
+        <p class="text-p2 pt-3 pb-1.5 px-5.5 font-bold light:text-black/50 dark:text-white/50" v-if="name">{{ name }}</p>
+        <slot />
+    </div>
+</template>
