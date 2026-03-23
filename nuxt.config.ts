c4fa9e9cae93042768d9ec1e469ff6125bd59fef
rldn12134 <rldn12134@rldn.xyz>
2026-03-24 00:31:11 +0900

    chore: 빌드 속도 개선을 위한 일부 설정 수정

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,6 +8,17 @@ export default defineNuxtConfig({
     compatibilityDate: "2025-07-15",
     devtools: { enabled: false },
 
+    future: {
+        compatibilityVersion: 4,
+    },
+    experimental: {
+        viteEnvironmentApi: true,
+    },
+    typescript: {
+        typeCheck: 'build',
+        strict: true
+    },
+
     vite: {
         plugins: [tailwindcss()],
         server: {
