a6e2ef32f88556476232d184400e20b60c8eeec0
기우12134 <rldn12134@rldn.xyz>
2026-03-24 03:28:26 +0000

    [ 서비스 ] 포인트 차감 Flow

---
+++ app/components/home/service.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import Search from "./search/index.vue";
+
+const session = useSession();
+</script>
+
+<template>
+    <div class="w-full flex items-center justify-between opacity-50 -mt-2">
+        <p class="text-ui-p1 font-medium  light:text-black/50 dark:text-white/50">{{ session!.name }}</p>
+        <NuxtLink to="/setting" v-slot="{ navigate }" custom>
+            <UButton @click="navigate()" color="neutral" variant="ghost" icon="i-ph-gear-fill" class="p-2 rounded-2xl hover:bg-accented active:bg-accented focus-visible:bg-accented" size="xl" />
+        </NuxtLink>
+    </div>
+    <div class="pb-2 mt-2.5 flex-1 flex flex-col">
+        <Search />
+    </div>
+</template>
