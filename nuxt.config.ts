8d9933a15b6b16850f835b3678b0762c9dda2031
rldn12134 <rldn12134@rldn.xyz>
2026-05-03 16:59:28 +0900

    feat: 대시보드 접근을 위한 /admin 프록시 구성

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -129,6 +129,9 @@ export default defineNuxtConfig({
         '/api/**': {
             proxy: process.env.NODE_ENV === 'production' ? 'http://backend:8000/**' : 'http://localhost:8000/**',
         },
+        '/admin/**': {
+            proxy: process.env.NODE_ENV === 'production' ? 'http://dashboard:5235/**' : 'http://localhost:5235/**',
+        },
     },
 
     runtimeConfig: {
