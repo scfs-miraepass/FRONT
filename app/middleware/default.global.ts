0a804853fb0e08e287816b4d4e25d9e227e4fd0a
rldn12134 <dyddns101213@naver.com>
2026-05-19 22:29:33 +0900

    feat: 페이지 이동 트렌지션과 로딩 인디케이터
    Closes #12

---
+++ app/middleware/default.global.ts
@@ -0,0 +1,8 @@
+export default defineNuxtRouteMiddleware((to, from) => {
+    if (import.meta.client) {
+        const isTransition = useState('isTransition', () => true)
+        isTransition.value = true
+
+        return new Promise((resolve) => setTimeout(resolve, 300))
+    }
+})
