fa87ede2b8d91206c72de9ebf067e7a9b79edec1
rldn12134 <dyddns101213@naver.com>
2026-07-13 20:40:56 +0900

    feat: 획득 스탬프 갯수 추가, 교사 스탬프 페이지 접근 추가

--- app/components/home/student.vue
+++ app/components/home/student.vue
@@ -20,7 +20,7 @@ const session = useSession()
             <NuxtLink to="/system/ranking" v-slot="{ navigate }" custom>
                 <PageButton @click="navigate()" icon="i-ph-ranking" label="포인트 순위" />
             </NuxtLink>
-            <NuxtLink to="/student/stamp" v-slot="{ navigate }" custom>
+            <NuxtLink to="/system/stamp" v-slot="{ navigate }" custom>
                 <PageButton @click="navigate()" icon="material-symbols-light:festival-rounded" label="'탄소중립 동아리 페스티벌' 스탬프" />
             </NuxtLink>
         </Section>
