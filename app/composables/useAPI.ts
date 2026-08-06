68c4ba4e2431eac01b5f90055ded0e273f38dac8
rldn12134 <dyddns101213@naver.com>
2026-08-06 06:14:19 +0000

    fix: useAPI가 정상적으로 동작하지 않던 문제 해결

--- app/composables/useAPI.ts
+++ app/composables/useAPI.ts
@@ -1,5 +1,4 @@
 import type { UseFetchOptions, KeysOf } from "nuxt/app";
-import { _fetch } from "./_fetch";
 
 export const useAPI = <T, ResT = T, Keys extends KeysOf<ResT> = KeysOf<ResT>>(
     url: string | (() => string),
@@ -7,6 +6,6 @@ export const useAPI = <T, ResT = T, Keys extends KeysOf<ResT> = KeysOf<ResT>>(
 ) => {
     return useFetch<T, ResT, Keys>(url, {
         ...options,
-        _fetch: _fetch as typeof globalThis.$fetch,
+        $fetch: _fetch as typeof globalThis.$fetch,
     });
 };
