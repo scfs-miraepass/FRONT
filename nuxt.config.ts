cc5333aa287da4d05b3850d032ccd0e24fefcaca
rldn12134 <rldn12134@rldn.xyz>
2026-08-06 23:06:40 +0900

    chore: 필요없는 패키지 제거및 업그레이드

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -123,7 +123,7 @@ export default defineNuxtConfig({
         },
     },
 
-    css: ["./app/assets/styles/main.css", "./app/assets/styles/tossface.css"],
+    css: ["~/assets/styles/main.css", "~/assets/styles/tossface.css"],
 
     routeRules: {
         '/api/**': {
