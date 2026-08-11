6e2f0a26a90dc2e404b7b4f374469d922f074a97
rldn12134 <dyddns101213@naver.com>
2026-08-11 17:09:06 +0900

    chore: 페이지 구조 변경

---
+++ app/pages/system/pointHistory.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <div class="flex items-center">
+        <NuxtLink to="/" v-slot="{ navigate }" custom>
+            <UButton @click="navigate()" color="neutral" variant="ghost" icon="i-ph-caret-left-bold" class="p-2 rounded-2xl hover:bg-accented active:bg-accented focus-visible:bg-accented" size="xl" />
+        </NuxtLink>
+        <p class="text-2xl font-bold text-gray-900 dark:text-white ml-1">탄소중립 동아리 페스티벌</p>
+    </div>
+    <div>
+
+    </div>
+</template>
